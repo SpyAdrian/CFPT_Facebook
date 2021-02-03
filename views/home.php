cdc78d8b0733a3dca9cc7f25ebc9775eab46308b
andria01 <Adriandria@hotmail.fr>
2021-02-03 14:49:05 +0100

    Chapitre 1.3 - Responsive

--- views/home.php
+++ views/home.php
@@ -22,23 +22,25 @@
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-                <ul class="navbar-nav me-5">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="post.php">Post</a>
-                    </li>
-                </ul>
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                    </svg>
+            <div class="collapse navbar-collapse" id="navbarMain">
+                <div class="d-flex justify-content-end w-100">
+                    <ul class="navbar-nav me-5">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="post.php">Post</a>
+                        </li>
+                    </ul>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                        </svg>
+                    </div>
                 </div>
             </div>
         </div>
@@ -49,8 +51,8 @@
 
     <!-- Banner -->
     <div class="w-100 h-40 bg-primary d-flex flex-column justify-content-center align-items-center">
-        <h1 class="display-1 pb-5">Welcome</h1>
-        <h1 class="display-6">From The Facebook CFPT</h1>
+        <h1 class="text-center display-1 pb-5">Welcome</h1>
+        <h1 class="text-center display-6">From The Facebook CFPT</h1>
     </div>
 
     <!-- Cards -->
